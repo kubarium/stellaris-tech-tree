5171c751df72913b82b7f723eec9ced5fe3c4d47
kubarium <iam@denizkumsal.com>
2017-07-28 15:55:11 -0400

    implements consolidation of feature data

--- src/components/Leaf.js
+++ src/components/Leaf.js
@@ -8,28 +8,28 @@ const Leaf = (props) => {
         "tech-bg-dangerous": props.data.is[0]
     })
     return (
-        <div className={leafBackground}>
-            <h1>
-                {props.data.name}
-            </h1>
-            <ul>
-                {props
-                    .data
-                    .feature_unlocks
-                    .map((feature, index) =>< li key = {
-                        index
-                    } > {
-                        feature
-                    } < /li>)}
-            </ul>
-            <img className="tech-img" src={`/images/technologies/${props.data.key}.png`}/>
-            <img
-                className="tech-category"
-                src={`/images/expertise/${props.data.category}.png`}/>
-            <img className="tech-area" src={`/images/research-${props.data.area}.png`}/> {props.data.is[2] > 0 && <img className="tech-tier" src="/images/repeatable.png"/>}
-            {props.data.tier > 0 && <img className="tech-tier" src={`/images/tier-${props.data.tier}.png`}/>}
+        <div className={ leafBackground }>
+          <h1>{ props.data.name }</h1>
+          <ul>
+            { props.data.feature_unlocks.map((feature, index) => {
+                  const unlockedComponents = RegExp("Unlocks Component", "g").exec(feature)
+              
+                  if (unlockedComponents) {
+                      console.log(unlockedComponents)
+                  }
+                  return
+                  <li key={ index }>
+                    { feature }
+                  </li>
+              }) }
+          </ul>
+          <img className="tech-img" src={ `/images/technologies/${props.data.key}.png` } />
+          <img className="tech-category" src={ `/images/expertise/${props.data.category}.png` } title={ props.data.category } />
+          <img className="tech-area" src={ `/images/research-${props.data.area}.png` } title={ props.data.area } />
+          { props.data.is[2] && <img className="tech-repeatable" src="/images/repeatable.png" title="Repeatable" /> }
+          { props.data.tier && <img className="tech-tier" src={ `/images/tier-${props.data.tier}.png` } title={ `Tier: ${props.data.tier}` } /> }
         </div>
-    );
+        );
 };
 
 export default Leaf;
